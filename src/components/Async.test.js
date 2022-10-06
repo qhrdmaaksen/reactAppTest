ed1ab81a0bdbadb3a49c6958268b16fca9930349
vitamin777 <rlaalsdn8@gmail.com>
2022-10-06 19:59:49 +0900

    test: mockResolvedValue 활용
    -window.fetch.mockResolvedValue 로 더미 함수에 모의 결과 값
    설정 후 정상적으로 렌더링하는지 테스트
    
    -test success

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -5,10 +5,11 @@ describe('Async component test', () => {
   test('요청 성공 시 포스트 가 렌더링 되는지 테스트', async () => {
     /*fetch Fn 은 window.fetch 로 사용*/
     window.fetch = jest.fn()/*fn method 는 더미 함수를 만들어줌*/
-    /*fetch 함수가 호출되었을 때 결정되어야 하는 값을 설정할 수 있게 해줌*/
-    /*window.fetch.mockResolevedValueOnce({
-
-    })*/
+    /*mockResolvedValue() fetch 함수가 호출되었을 때 결정되어야 하는 값을 설정할 수 있게 해줌*/
+    /*내장 fetch 함수를 더미 fetch 함수로 덮어쓰고 있는 것*/
+    window.fetch.mockResolvedValue({
+      json: async()=>[{id: 'p1',title: 'First post'}]
+    })
     render(<Async />);
     const listItemElement = await screen.findAllByRole('listitem');
     /*기대하는 바는 listItemElements 가 빈 배열이 아니란 것*/
